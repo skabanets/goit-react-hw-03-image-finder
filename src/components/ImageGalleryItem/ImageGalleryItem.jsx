98d381cb03ef21bc9e4c3fe3cb712e3f15af55a4
Sergey Kabanets <skabanets97@gmail.com>
2024-01-12 14:07:31 +0200

    fix issue with request

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import s from './imageGalleryItem.module.css';
 
 export const ImageGalleryItem = ({ content, openModal }) => {
-  const { webformatURL, largeImageURL } = content;
+  const { webformatURL, largeImageURL, tags } = content;
   return (
     <li className={s.ImageGalleryItem} onClick={() => openModal(largeImageURL)}>
-      <img src={webformatURL} alt="" className={s.ImageGalleryItemImage} />
+      <img src={webformatURL} alt={tags} className={s.ImageGalleryItemImage} />
     </li>
   );
 };
