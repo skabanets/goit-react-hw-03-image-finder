24d1790edc902ca36d6551fa2aeb0aa552b4eed7
Sergey Kabanets <skabanets97@gmail.com>
2024-01-12 12:31:54 +0200

    add searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
-export const App = () => {
-  return (
-    <>
-      <div>React homework template</div>
-    </>
-  );
-};
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit} />
+      </>
+    );
+  }
+}
